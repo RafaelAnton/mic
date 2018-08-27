1e9b15e1564af201bcad431f899987163bbde513
Chente <vcatacora@altabpo.com>
2018-08-27 12:37:17 -0500

    hola

--- mic_equipos/models/__init__.py
+++ mic_equipos/models/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from . import models
-from . import equipo_caracteristicas
 from . import mic_caracteristica
+from . import equipo_caracteristicas
 from . import mic_equipo
