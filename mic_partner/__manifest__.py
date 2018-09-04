50172c7527857b4e3afe185af98ee07551c4f270
Chente <vcatacora@altabpo.com>
2018-09-04 11:48:17 -0500

    holix3

--- mic_partner/__manifest__.py
+++ mic_partner/__manifest__.py
@@ -27,7 +27,7 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
-        'views/views_cliente',
+        'views/views_cliente.xml',
 
     ],
     # only loaded in demonstration mode
