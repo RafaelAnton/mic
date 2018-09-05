88cb0d31ce20721e3275819333dea8465ff42a77
Chente <vcatacora@altabpo.com>
2018-09-05 12:14:01 -0500

    franciscorregido

--- mic_suscription/models/mic_suscription.py
+++ mic_suscription/models/mic_suscription.py
@@ -14,6 +14,24 @@ class SucriptionMic(models.Model):
                                                related="cabecera_id.equipo_caracteristica")
     cabecera_estado = fields.Selection(related="cabecera_id.estado")
     cabecera_motivo = fields.Selection(related="cabecera_id.motivo")
-    comision = fields.Float(compute='compute_comision_sucription', string="Comision", store=True,
+    comision = fields.Float(computed='compute_comision_sucription', string="Comision", store=True,
                             track_visibility='onchange')
 
+    @api.onchange('cabecera_id')
+    def onchange_cabecera_id(self):
+        for suscription in self:
+            suscription.recurring_invoice_line_ids = [(0, False, {
+                u'product_id': suscription.cabecera_id.signal_id.id,
+                u'name': suscription.cabecera_id.signal_id.name,
+                u'actual_quantity': 1,
+                u'quantity': 1,
+                u'sold_quantity': 1,
+                u'price_unit': suscription.cabecera_id.signal_id.list_price
+            })]
+
+    @api.depends('recurring_total')
+    def compute_comision_sucription(self):
+        for suscription in self:
+            comision = ((
+                        suscription.recurring_total - suscription.partner_id.retencion)) * suscription.cabecera_id.signal_id.porcentaje_interes
+            suscription.comision = comision
