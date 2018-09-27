f49e3186c4ab56271c65a2a2d9a80092e4da38cc
Chente <vcatacora@altabpo.com>
2018-09-27 11:20:33 -0500

    holi2

--- mic_suscription/__manifest__.py
+++ mic_suscription/__manifest__.py
@@ -20,14 +20,12 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'sale_contract', 'mic_equipos','mic_partner'],
+    'depends': ['base', 'sale_contract', 'mic_equipos', 'mic_partner'],
 
     # always loaded
     'data': [
-        #'security/ir.model.access.csv',
+        # 'security/ir.model.access.csv',
         'views/views_sucription.xml',
-
-
 
     ],
     # only loaded in demonstration mode
