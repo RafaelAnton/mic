7e7d3ee6eec7a2122a3773263387d0036f407875
Chente <vcatacora@altabpo.com>
2018-10-11 11:34:18 -0500

    cambios

--- mic_suscription/__manifest__.py
+++ mic_suscription/__manifest__.py
@@ -25,7 +25,7 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views_sucription.xml',
+       # 'views/views_sucription.xml',
 
     ],
     # only loaded in demonstration mode
